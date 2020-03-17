fb49e113904dbf8107714ca2e10215578edd2229
rmuddu1 <ramchander.muddu@gmail.com>
2020-03-16 18:29:48 -0700

    Arrays:LargestNum

---
+++ src/interviewbit/arrays/LargestNumber2.java
@@ -0,0 +1,22 @@
+package interviewbit.arrays;
+
+import java.util.PriorityQueue;
+
+public class LargestNumber2 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public int findKthLargest(int[] nums, int k) {
+	    PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
+	    for(int i: nums){
+	        q.offer(i);
+	 
+	        if(q.size()>k){
+	            q.poll();
+	        }
+	    }
+	 
+	    return q.peek();
+	}
+}
